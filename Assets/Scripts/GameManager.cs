52c5a74e85f858696bfbed0cd9fdb9b151bb7196
Nils Sanderson <nils@learningpool.com>
2017-10-11 08:14:58 +0100

    Initial mvp

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public Material[] colors;
+	public float stayLit;
+	public float stayLitCounter;
+
+	private int colorSelect;
+
+
+	// Use this for initialization
+	void Start () {
+		StartGame ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// stayLitCounter is greater than 0 decrement it over time
+		if (stayLitCounter > 0) {
+			stayLitCounter -= Time.deltaTime;
+		} else {
+			// Set this colors alpha to half
+			colors[colorSelect].color = new Color(colors[colorSelect].color.r, colors[colorSelect].color.g, colors[colorSelect].color.b, .5f);
+		}
+	}
+
+	public void StartGame() {
+		// Set a random color
+		colorSelect = Random.Range(0, colors.Length);
+		// Set this colors alpha to full
+		colors[colorSelect].color = new Color(colors[colorSelect].color.r, colors[colorSelect].color.g, colors[colorSelect].color.b, 1f);
+
+		stayLitCounter = stayLit;
+	}
+
+	public void ColorPressed(int whichButton) {
+		if (colorSelect == whichButton) {
+			Debug.Log ("Correct");
+		} else {
+			Debug.Log ("Wrong");
+		}
+	}
+}
---
+++ Assets/Scripts/SelectionHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SelectionHandler : MonoBehaviour {
+
+	public int thisButtonNumber;
+	public bool selected;
+
+	private Material theMaterial;
+	private GameManager theGM;
+
+
+	// Use this for initialization
+	void Start () {
+		theMaterial = GetComponent<Renderer>().material;
+		theGM = FindObjectOfType<GameManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Something entered me: " + other);
+		if (other.gameObject.tag == "hand") {
+			theMaterial.color = new Color(theMaterial.color.r, theMaterial.color.g, theMaterial.color.b, 1f);
+			theGM.ColorPressed (thisButtonNumber);
+			this.selected = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		Debug.Log ("Something exited me: " + other);
+		if (other.gameObject.tag == "hand") {
+			theMaterial.color = new Color(theMaterial.color.r, theMaterial.color.g, theMaterial.color.b, .5f);
+			this.selected = false;
+		}
+	}
+}
