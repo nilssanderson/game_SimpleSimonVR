7a823c7781277582854a55c84dcfb5744266e3c4
BOOMCLAP-VR\HOME <info@davidbaxteraudio.com>
2017-10-13 22:39:32 +0100

    trying to get wwise sounds to work

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,36 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_1_Y_NOTIFICATION = 1211727895U;
+        static const AkUniqueID PLAY_2_G_NOTIFICATION = 1813395792U;
+        static const AkUniqueID PLAY_3_R_NOTIFICATION = 3490253340U;
+        static const AkUniqueID PLAY_4_B_NOTIFICATION = 3076200897U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SIMPLESIMONVR = 2768628195U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
