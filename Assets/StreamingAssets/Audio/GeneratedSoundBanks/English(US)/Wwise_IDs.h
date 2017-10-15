356e0c97be86e45a58adc61564f41236bd536d4f
David Baxter <info@davidbaxteraudio.com>
2017-10-15 17:27:32 +0100

    Audio assets added in Wwise

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/English(US)/Wwise_IDs.h
@@ -0,0 +1,46 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_1_Y_NOTIFICATION = 1211727895U;
+        static const AkUniqueID PLAY_2_G_NOTIFICATION = 1813395792U;
+        static const AkUniqueID PLAY_3_R_NOTIFICATION = 3490253340U;
+        static const AkUniqueID PLAY_4_B_NOTIFICATION = 3076200897U;
+        static const AkUniqueID PLAY_AMB_MEADOW_1SHOT_BIRDSING_RNDM = 132947601U;
+        static const AkUniqueID PLAY_AMB_MEADOW_1SHOT_BIRDSING_RNDM_01 = 2669103773U;
+        static const AkUniqueID PLAY_X_CORRECT_RNDM_01 = 1458294881U;
+        static const AkUniqueID PLAY_X_CORRECT_RNDM_02 = 1458294882U;
+        static const AkUniqueID PLAY_X_CORRECT_RNDM_03 = 1458294883U;
+        static const AkUniqueID PLAY_X_CORRECT_RNDM_04 = 1458294884U;
+        static const AkUniqueID PLAY_X_INCORRECT_RNDM_01 = 2198543598U;
+        static const AkUniqueID PLAY_X_INCORRECT_RNDM_02 = 2198543597U;
+        static const AkUniqueID PLAY_X_INCORRECT_RNDM_03 = 2198543596U;
+        static const AkUniqueID PLAY_X_INCORRECT_RNDM_04 = 2198543595U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SIMPLESIMONVR = 2768628195U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
